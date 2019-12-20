90a621328577475e9c3cf9744502985f7a2cdcbd
Mateusz Wojcik <mateuszwojcik19@gmail.com>
2019-12-20 20:10:40 +0100

    Add second svc

---
+++ MainframeSvc/MainServices/DataService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MainServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DataService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DataService.svc or DataService.svc.cs at the Solution Explorer and start debugging.
+    public class DataService : IDataService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
--- MainframeSvc/MainServices/IAccountService.cs
+++ MainframeSvc/MainServices/IAccountService.cs
@@ -8,9 +8,8 @@ using System.Text;
 
 namespace MainServices
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IAccountService
     {
 
         [OperationContract]
@@ -19,11 +18,8 @@ namespace MainServices
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
-        // TODO: Add your service operations here
     }
 
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
     public class CompositeType
     {
