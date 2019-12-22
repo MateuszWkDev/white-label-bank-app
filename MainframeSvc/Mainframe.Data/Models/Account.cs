12486279b1ad4a8e808e351c63dc37bb3e03de58
Mateusz Wojcik <mateuszwojcik19@gmail.com>
2019-12-22 15:34:11 +0100

    Add models for transactions and accounts

---
+++ MainframeSvc/Mainframe.Data/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Mainframe.Data.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ MainframeSvc/Mainframe.Data/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mainframe.Data.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FromAccountId { get; set; }
+        public int ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Account FromAccount { get; set; }
+        public virtual Account ToAccount { get; set; }
+    }
+}
