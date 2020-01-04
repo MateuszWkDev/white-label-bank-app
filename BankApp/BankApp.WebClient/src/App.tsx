2230c24c267e73be9af0c489a5d58b9e657a63f1
Mateusz Wojcik <mateuszwojcik19@gmail.com>
2020-01-04 14:33:39 +0100

    Fix errors and warnings

--- BankApp/BankApp.WebClient/src/App.tsx
+++ BankApp/BankApp.WebClient/src/App.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div
-      tabIndex={0}
-      role="button"
-      onClick={e => console.log(e)}
-      onKeyPress={e => console.log(e)}
-    >
-      TEST
-    </div>
-  );
+  return <div>TEST</div>;
 };
 
 export default App;
