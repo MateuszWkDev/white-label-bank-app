fea271303534ac7b4a5f8b02b617cebe6f42c3d8
Mateusz Wojcik <mateuszwojcik19@gmail.com>
2020-01-06 18:16:54 +0100

    Prepare cors for web app

--- BankApp/BankApp.WebClient/src/AppConfig.tsx
+++ BankApp/BankApp.WebClient/src/AppConfig.tsx
@@ -1,13 +1,19 @@
+const isOnTest = window.location.href.includes('test-wlba');
 class AppConfig {
   static defaultLang = 'en';
 
   static defaultCompany = 'default';
 
-  static contentServerUrl = 'http://localhost:50312';
+  static contentServerUrl = isOnTest
+    ? 'https://contentapi-test-wlba.azurewebsites.net'
+    : 'http://localhost:50312';
 
-  static bankApiUrl = 'http://localhost:61493/api';
+  static bankApiUrl = isOnTest
+    ? 'https://bankapi-test-wlba.azurewebsites.net/api'
+    : 'http://localhost:61493/api';
 
-  static baseMediaUrl = 'http://localhost:50312/media';
+  static baseMediaUrl = isOnTest
+    ? 'https://contentapi-test-wlba.azurewebsites.net/media'
+    : 'http://localhost:50312/media';
 }
-
 export default AppConfig;
