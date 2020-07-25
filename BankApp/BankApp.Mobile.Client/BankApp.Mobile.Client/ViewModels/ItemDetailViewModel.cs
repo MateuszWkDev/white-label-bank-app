909ad68978d712baa519baa86270d9b21936c827
Mateusz Wojcik <mateuszwojcik19@gmail.com>
2020-07-25 11:11:48 +0200

    Add mobile app project

---
+++ BankApp/BankApp.Mobile.Client/BankApp.Mobile.Client/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BankApp.Mobile.Client.Models;
+
+namespace BankApp.Mobile.Client.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
